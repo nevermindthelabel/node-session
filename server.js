e420564a46ac8e7e0ace58c4fe7b0942c1a186d4
Matt Kilcup <nevermindthelabel@gmail.com>
2019-11-11 20:32:10 -0700

    added mongodb and mongoose

--- server.js
+++ server.js
@@ -3,55 +3,40 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
 const routes = require('./routes');
+
+mongoose.connect(
+  process.env.mongodb,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  () => console.log('mongo db connected')
+);
 
 const PORT = 4000 || process.env.PORT;
 
 const app = express();
 
-// app.use(routes);
+app.use(routes);
+app.use(helmet());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(
-  session(
-    {
-      key: process.env.key,
-      secret: process.env.secret,
-      resave: process.env.resave,
-      saveUninitialized: process.env.Uninitialized,
-      cookie: {
-        // expires: process.env.cookieExpiration,
-        // maxAge: process.env.maxAge,
-        httpOnly: true,
-      },
-    },
-    console.log(process.env.cookieExpiration)
-  )
+  session({
+    key: process.env.key,
+    secret: process.env.secret,
+    resave: process.env.resave,
+    saveUninitialized: process.env.Uninitialized,
+    cookie: {
+      httpOnly: true
+    }
+  })
 );
-
-app.use((req, res, next) => {
-  if (!req.cookies.user_sid) {
-    res.clearCookie('user_sid');
-  }
-  next();
-});
-
-const checkUser = (req, res, next) => {
-  if (req.cookies.user_sid) {
-    res.send('logged in');
-  } else {
-    res.send('not logged in');
-  }
-};
-
-app.get('/', checkUser, (req, res) => {
-  res.status(200);
-});
-
-app.get('/login', (req, res) => {
-  res.cookie('user_sid', true).json('cookie');
-});
 
 app.listen(PORT, () =>
   console.log(`connected on port http://localhost:${PORT}`)
